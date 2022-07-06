31a58c61d4538d5b24aa3d82c0bb6d2fddf9f184
Yago <peixinhoyago@gmail.com>
2022-07-06 17:28:08 -0300

    :sparkles: feat: new advanced custom fields in page sobre

--- wp-content/themes/bikcraft/page-sobre.php
+++ wp-content/themes/bikcraft/page-sobre.php
@@ -9,21 +9,19 @@
 		<section class="missao_sobre container animar-interno">
 			<div class="grid-10">
 				<h2 class="subtitulo-interno">História, Missão e Visão</h2>
-				<p>Quando iniciamos a Bikcraft queriamos apenas um produto que adoraríamos utilizar. Eramos apaixonados por pedalar e também por fazer as coisas com as nossas próprias mãos. Assim surgiu um sonho na garagem da nossa casa.</p>
-				<p>Conheça os nossos produtos, pergunte para os nossos clientes e descubra a maravilha de ter uma Bikcraft na sua casa.</p>
+				<p><?php the_field('missao')?></p>
 			</div>
 			<div class="grid-6">
 				<h2 class="subtitulo-interno">Valores</h2>
 				<ul>
-					<li>- Qualidade no processo com</li>
-					<li>- Foco no cliente sem perder a</li>
-					<li>- Diversão, preservando a</li>
-					<li>- Natureza com sustentabilidade</li>
+					<?php if(have_rows('valores_lista')) : while(have_rows('valores_lista')) : the_row(); ?>
+						<li>- <?php the_sub_field('valor')?></li>
+					<?php endwhile; else: endif;?>
 				</ul>
 			</div>
 
 			<div class="grid-16 foto-equipe">
-				<img src="img/equipe-bikcraft.jpg" alt="Equipe Bikcraft">
+				<img src="<?php the_field('imagem_equipe'); ?>" alt="Equipe Bikcraft">
 			</div>
 
 		</section>
