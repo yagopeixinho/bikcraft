a188f84170a2914e208a2087291bb7e4aa000bc9
Yago <peixinhoyago@gmail.com>
2022-07-07 17:17:36 -0300

    :sparkles: feat: new advanced customs fields on footer

--- wp-content/themes/bikcraft/footer.php
+++ wp-content/themes/bikcraft/footer.php
@@ -1,56 +1,82 @@
-		<footer>
-			<div class="footer">
-				<div class="container">
+<?php
+$contato = get_page_by_title('contato');
+?>
+<style type="text/css">
+	.quebra {
+		background: url("<?php the_field('background_footer', $contato) ?>") no-repeat center;
+		background-size: cover;
+	}
+</style>
 
-					<div class="grid-8 footer_historia">
-						<h3>Nossa História</h3>
-						<p>Quando iniciamos a Bikcraft queriamos apenas um produto que adoraríamos utilizar. Eramos apaixonados por pedalar e também por fazer as coisas com as nossas próprias mãos. Assim surgiu um sonho na garagem da nossa casa.</p>
-					</div>
+<div class="quebra">
+	<blockquote class="quote-externo container">
+		<p><?php the_field('frase_footer', $contato) ?></p>
+		<cite><?php the_field('autor_footer', $contato) ?></cite>
+	</blockquote>
+</div>
+<footer>
+	<div class="footer">
+		<div class="container">
 
-					<div class="grid-4 footer_contato">
-						<h3>Contato</h3>
-						<ul>
-							<li>- 21 9999-9999</li>
-							<li>- contato@bikcraft.com</li>
-							<li>- Botafago - RJ</li>
-						</ul>
-					</div>
-
-					<div class="grid-4 footer_redes">
-						<h3>Redes Sociais</h3>
-						<?php include(TEMPLATEPATH . '/inc/redes-sociais.php'); ?>
-					</div>
-
-				</div>
+			<div class="grid-8 footer_historia">
+				<h3>Nossa História</h3>
+				<p><?php the_field('resumo_historia', $contato) ?></p>
 			</div>
 
-			<div class="copy">
-				<div class="container">
-					<p class="grid-16">Bikcraft 2015 - Alguns direitos reservados.</p>
-				</div>
+			<div class="grid-4 footer_contato">
+				<h3>Contato</h3>
+				<ul>
+					<li>- <?php the_field('telefone', $contato); ?></li>
+					<li>- <?php the_field('email', $contato); ?></li>
+					<li>-<?php the_field('endereco1', $contato); ?></li>
+				</ul>
 			</div>
-		</footer>
 
-	<!-- JavaScript -->
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="js/libs/jquery-1.11.2.min.js"><\/script>')</script>
-	<script src="js/plugins.js"></script>
-	<script src="js/main.js"></script>
-	<!-- JavaScript -->
+			<div class="grid-4 footer_redes">
+				<h3>Redes Sociais</h3>
+				<?php include(TEMPLATEPATH . '/inc/redes-sociais.php'); ?>
+			</div>
 
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		</div>
+	</div>
 
-	  ga('create', 'UA-60088262-1', 'auto');
-	  ga('send', 'pageview');
-	</script>
+	<div class="copy">
+		<div class="container">
+			<p class="grid-16"><?php bloginfo('name') ?> <?php echo date('Y'); ?> - Alguns direitos reservados.</p>
+		</div>
+	</div>
+</footer>
 
-    <!-- Início WordPress Footer -->
-      <?php wp_footer(); ?>
-    <!-- Final WordPress Footer -->
+<!-- JavaScript -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script>
+	window.jQuery || document.write('<script src="js/libs/jquery-1.11.2.min.js"><\/script>')
+</script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+<!-- JavaScript -->
 
-	</body>
+<script>
+	(function(i, s, o, g, r, a, m) {
+		i['GoogleAnalyticsObject'] = r;
+		i[r] = i[r] || function() {
+			(i[r].q = i[r].q || []).push(arguments)
+		}, i[r].l = 1 * new Date();
+		a = s.createElement(o),
+			m = s.getElementsByTagName(o)[0];
+		a.async = 1;
+		a.src = g;
+		m.parentNode.insertBefore(a, m)
+	})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+	ga('create', 'UA-60088262-1', 'auto');
+	ga('send', 'pageview');
+</script>
+
+<!-- Início WordPress Footer -->
+<?php wp_footer(); ?>
+<!-- Final WordPress Footer -->
+
+</body>
+
 </html>
